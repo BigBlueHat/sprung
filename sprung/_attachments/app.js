7f68ae316b2809179872137553247ff6619bb9f8
BigBlueHat <byoung@bigbluehat.com>
2014-06-01 21:28:53 -0400

    3 lists to browse all of it

--- sprung/_attachments/app.js
+++ sprung/_attachments/app.js
@@ -70,38 +70,72 @@ Vue.component('thing-type-list', {
       };
       xhr.send();
     }
-  },
+  }
 });
 
 Vue.component('thing-list', Fetchable.extend({
   data: {
     notebook: false,
-    type: 'Book'
+    type: false
   },
   computed: {
     apiUrl: function() {
-      if (this.notebook) {
+      if (this.notebook !== false && this.type !== false) {
+        // things of a certain kind from a certain place
         return '_view/by_notebook?reduce=false'
           + '&startkey=["' + this.notebook + '", "' + this.type + '"]'
           + '&endkey=["' + this.notebook + '", "' + this.type + '", {}]'
           + '&include_docs=true';
+      } else if (this.notebook !== false && this.type === false) {
+        // all the things from a certain place
+        return '_view/by_notebook?reduce=false'
+          + '&startkey=["' + this.notebook + '"]'
+          + '&endkey=["' + this.notebook + '", {}]'
+          + '&include_docs=true';
+      } else if (this.notebook === false && this.type !== false) {
+        // all the things of a certain type from all the places
+        return '_view/by_type?startkey=["' + this.type + '"]&endkey=["' +
+          this.type + '",{}]&reduce=false&include_docs=true';
+      } else {
+        // all the things from all the places
+        return '_view/by_type?reduce=false';
       }
-      return '_view/by_type?startkey=["' + this.type + '"]&endkey=["' +
-        this.type + '",{}]&reduce=false&include_docs=true';
     }
   },
   created: function() {
     this.$watch('type', function() {
       this.fetchData();
     });
+  },
+  methods: {
+    fetchData: function () {
+      if (!this.apiUrl) return false;
+      var xhr = new XMLHttpRequest(),
+          self = this;
+      xhr.open('GET', self.apiUrl);
+      xhr.onload = function () {
+        self.items = JSON.parse(xhr.responseText);
+        if (self.notebook === false && self.type === false) {
+          // we've got a non-include_docs thing, so let's spoof `name`
+          for (var i = 0; i < self.items.rows.length; i++) {
+            if (self.items.rows[i].key.length === 2) {
+              self.items.rows[i].doc = {};
+              self.items.rows[i].doc.name = self.items.rows[i].key[1];
+            }
+          }
+        }
+      };
+      xhr.send();
+    }
   }
 }));
+
 var app = new Vue({
   el: 'body',
   data: {
     current: {
       notebook: false,
-      type: 'Task'
+      type: false
     }
   }
 });
