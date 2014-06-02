ed0241de19826c0b0abbb9e09fe8829774b37bd8
BigBlueHat <byoung@bigbluehat.com>
2014-06-01 22:13:40 -0400

    dropdown variant for notebook-list

--- sprung/_attachments/app.js
+++ sprung/_attachments/app.js
@@ -21,6 +21,16 @@ var Fetchable = Vue.extend({
 Vue.component('thing-notebook-list', Fetchable.extend({
   data: {
     apiUrl: '_view/notebooks?reduce=false'
+  },
+  methods: {
+    setCurrent: function(notebook) {
+      this.$parent.current.notebook = notebook.id;
+      this.$parent.current.notebook_name = notebook.key;
+    },
+    unsetCurrent: function() {
+      this.$parent.current.notebook = false;
+      this.$parent.current.notebook_name = false;
+    }
   }
 }));
 
