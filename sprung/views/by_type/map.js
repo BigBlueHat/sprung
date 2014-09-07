be679654077db41f38ce163b2ef0c8bb95df0466
BigBlueHat <byoung@bigbluehat.com>
2014-09-07 17:09:25 -0400

    Avoid `object` typed documents (schemas)

--- sprung/views/by_type/map.js
+++ sprung/views/by_type/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  if ('type' in doc && doc.type != 'Notebook') {
+  if ('type' in doc && doc.type !== 'Notebook' && doc.type !== 'object') {
     emit([doc.type, doc.name], 1);
   }
 }
