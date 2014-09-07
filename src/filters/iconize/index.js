ff91c3ccd87bc8dd313a4c5788e6c9cf265929de
BigBlueHat <byoung@bigbluehat.com>
2014-09-07 19:20:06 -0400

    An icon for every Thing

--- src/filters/iconize/index.js
+++ src/filters/iconize/index.js
@@ -2,12 +2,19 @@ module.exports = function(v, k) {
   var l = v.toLowerCase();
   // TODO: ugh...this should be pulled out somewher
   var type_to_icon = {
+    alarm: 'time',
+    album: 'music',
     business: 'building',
     checklist: 'list',
+    contact: 'phone sign',
     'event': 'calendar',
+    movie: 'video',
     note: 'text file',
-    product: 'dollar basic',
-    task: 'checkmark sign'
+    product: 'money',
+    recipe: 'browser',
+    task: 'checkmark sign',
+    'tv show': 'desktop',
+    wine: 'glass'
   };
   return type_to_icon[l] || l;
 };
