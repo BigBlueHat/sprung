55f2457b0f41a53fe17aece5b3062984a95b8728
BigBlueHat <byoung@bigbluehat.com>
2015-03-07 00:37:15 -0500

    Add delete button to thing-modal

--- src/thing-modal/index.js
+++ src/thing-modal/index.js
@@ -1,4 +1,7 @@
 var Vue = require('vue');
+var PouchDB = require('pouchdb');
+
+var db = new PouchDB('http://localhost:5984/sprung/');
 
 module.exports = Vue.extend({
   template: require('./template.html'),
@@ -17,6 +20,16 @@ module.exports = Vue.extend({
   methods: {
     destroy: function() {
       this.$destroy(true);
+    },
+    remove: function() {
+      var self = this;
+      db.remove(self.$data._id, self.$data._rev, function(err, resp) {
+        if (err) {
+          console.log('error: ', err);
+        } else {
+          self.$destroy(true);
+        }
+      });
     }
   },
   components: {
