79926aff737e6c8743f1874aef150d943fb40732
BigBlueHat <byoung@bigbluehat.com>
2015-04-04 16:15:13 -0400

    Setup gulp.js at long last
    `npm run dev` builds sprung and publishes it to `default`
    in the new `config.json` (or to `--url {URL}` if provided.
    
    `npm run docs` builds and publishes the type docs.

--- README.md
+++ README.md
@@ -21,39 +21,19 @@ Web" and is therefore quite concious of such needs.
 **Note:** I fully intend to make this all much easier. "Geeks" only atm.
 Sorry. :frown:
 
-1. Create a `.couchapprc` JSON doc in the `sprung` folder, something like this:
-```
-{
-  "env": {
-    "default": {
-      "db": "http://USER:PASSWORD@localhost:5984/sprung"
-    }
-  }
-}
-```
-(obviously replace `USER` and `PASSWORD` with something useful...)
-2. Install [couchapp.py](http://github.com/couchapp/couchapp)
-(or other `couchapp.py` compatible thing).
+1. Copy `config.json.sample` to `config.json`
+2. Update the URL to match your settings.
 3. Next install the necessary dev tools and dependencies via:
 ```
 npm install
 ```
-4. Build the `app.js` file:
+4. Build Sprung and the type definitions and publish them:
 ```
-npm run dev
-```
-5. Deploy the CouchApp:
-```
-cd sprung
-couchapp push . default
+gulp
 ```
 
-Change the URL above to the be the URL of the CouchDB or
-[Cloudant](http://cloudant.com/) database you'd like to keep your Springpad
-data in.
-
-Once that's done, visit the URL `couchapp.py` gives you. Which will be
-something like: `http://localhost:5984/sprung/_design/sprung/index.html`
+Once that's done, visit the URL based on your CouchDB URL, such as:
+`http://localhost:5984/sprung/_design/sprung/index.html`
 
 ## oEmbed
 
---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var fs = require('fs');
+
+var browserify = require('browserify');
+var glob = require('glob');
+var gulp = require('gulp');
+var partialify = require('partialify');
+var push = require('couch-push');
+var source = require('vinyl-source-stream');
+
+var argv = require('yargs').argv;
+var config = require('./config.json');
+var couch_url;
+if (argv.url) {
+  couch_url = argv.url;
+} else if (config.env && config.env['default'] && config.env['default'].db) {
+  couch_url = config.env['default'].db;
+} else {
+  // TODO: make this hault
+  console.log('You must supply the URL to your CouchDB instance (via --url or config.json');
+}
+
+gulp.task('sprung', function() {
+  var b = browserify({
+    entries: './src/main.js',
+    debug: true,
+    transform: [partialify]
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./_design/sprung/_attachments/'));
+});
+
+gulp.task('docs', function() {
+  glob('_docs/*', function(err, matches) {
+    if (err) throw err;
+
+    matches.forEach(function(doc) {
+      var type = doc.split('~')[0];
+      if (type === '_docs/type' && fs.existsSync(doc + '/index.js')) {
+        // we have a type definition, build its component
+        browserify({
+          entries: './' + doc + '/index.js',
+          debug: true,
+          transform: [partialify]
+        })
+        .bundle()
+        .pipe(source('component.js'))
+        .pipe(gulp.dest('./' + doc + '/_attachments/'));
+      }
+
+      push(couch_url, doc,
+        function(err, resp) {
+          if (err) throw err;
+          console.log(resp);
+        });
+    });
+  });
+});
+
+gulp.task('apps', function() {
+  glob('_design/*', function(err, matches) {
+    if (err) throw err;
+    matches.forEach(function(ddoc) {
+      push(couch_url, ddoc,
+        function(err, resp) {
+          if (err) throw err;
+          console.log(resp);
+        });
+    });
+  });
+});
+
+gulp.task('default', ['sprung', 'apps', 'docs']);
