fa7297336475bf2f1b54f5e32dbb066848821394
BigBlueHat <byoung@bigbluehat.com>
2015-06-23 14:37:52 -0400

    Switch type list to PouchDB

--- src/thing-type-list/index.js
+++ src/thing-type-list/index.js
@@ -1,3 +1,10 @@
+var PouchDB = require('pouchdb');
+// TODO: move this to a config lib
+var db_name = location.pathname.split('/')[1];
+var db_url = location.protocol + '//' + location.hostname
+    + (location.port ? ':' + location.port : '') + '/' + db_name + '/';
+var db = new PouchDB(db_url);
+
 module.exports = {
   replace: true,
   template: require('./template.html'),
@@ -25,29 +32,42 @@ module.exports = {
   },
   methods: {
     fetchData: function () {
-      if (!this.apiUrl) return false;
-      var xhr = new XMLHttpRequest(),
-          self = this;
-      xhr.open('GET', self.apiUrl);
-      xhr.onload = function () {
-        self.items = JSON.parse(xhr.responseText);
-        self.types = [];
-        if (self.notebook) {
-          for (var i = 0; i < self.items.rows.length; i++) {
-            // if there are 2 keys, we have a type entry
-            if (self.items.rows[i].key.length === 2) {
-              self.types.push({"type": self.items.rows[i].key[1],
+      var self = this;
+      var view = '';
+      var params = {};
+      if (this.notebook) {
+        view = 'by_notebook';
+        params = {
+          group_level: 2,
+          startkey: [this.notebook],
+          endkey: [this.notebook, {}]
+        };
+      } else {
+        view = 'by_type';
+        params = {
+          group_level: 1
+        };
+      }
+
+      db.query('sprung/' + view, params)
+        .then(function(resp) {
+          self.items = resp;
+          self.types = [];
+          if (self.notebook) {
+            for (var i = 0; i < self.items.rows.length; i++) {
+              // if there are 2 keys, we have a type entry
+              if (self.items.rows[i].key.length === 2) {
+                self.types.push({"type": self.items.rows[i].key[1],
+                  "count": self.items.rows[i].value});
+              }
+            }
+          } else {
+            for (var i = 0; i < self.items.rows.length; i++) {
+              self.types.push({"type": self.items.rows[i].key[0],
                 "count": self.items.rows[i].value});
             }
           }
-        } else {
-          for (var i = 0; i < self.items.rows.length; i++) {
-            self.types.push({"type": self.items.rows[i].key[0],
-              "count": self.items.rows[i].value});
-          }
-        }
-      };
-      xhr.send();
+        });
     }
   }
 };
